b3c92eebcb3032af5c6803afa40c338e957ea782
Niklas Klasen <klasen.niklas@gmail.com>
2025-07-01 15:09:30 +0200

    remove unneeded module

--- Update-AzureFirewallPolicy.ps1
+++ Update-AzureFirewallPolicy.ps1
@@ -3,6 +3,5 @@ Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Running 'Update-A
 Write-Host ""
 
 # Import & Install Modules
-Install-Module -Name powershell-yaml -AllowClobber -Force
 if ([bool](Get-Module -Name module.afwManagement -ErrorAction SilentlyContinue)) { Remove-Module -Name module.afwManagement }
 Import-Module ".\module.afwManagement"

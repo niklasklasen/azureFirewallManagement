d0b4b98bd63c9e1fd37d08b708af7bafc7bef46b
Niklas Klasen <klasen.niklas@gmail.com>
2025-07-04 00:21:32 +0200

    build rule collection groups object

--- module.afwManagement/public/ConvertTo-RuleCollectionGroupObject.ps1
+++ module.afwManagement/public/ConvertTo-RuleCollectionGroupObject.ps1
@@ -2,14 +2,57 @@ function ConvertTo-RuleCollectionGroupObject {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [string]$Name,
-
-        [Parameter(Mandatory = $true)]
-        [int]$Priority
+        [string]$FirewallRulesFolderPath
     )
-convert
-    # Create the Rule Collection Group object
-    $ruleCollectionGroup = New-AzFirewallPolicyRuleCollectionGroup -Name $Name -Priority $Priority
 
-    return $ruleCollectionGroup
+    $ruleCollectionGroupFolders = Get-ChildItem -Path ./firewall-rules -Directory -Recurse
+
+    $ruleCollectionGroups = @() 
+
+    foreach ($ruleCollectionGroup in $ruleCollectionGroupFolders) {
+        $RuleCollectionGroupName = $ruleCollectionGroup.Name.Split("-")[1]
+        $RuleCollectionGroupPriority = $ruleCollectionGroup.Name.Split("-")[0]
+
+        $ruleCollections = Get-ChildItem -Path $ruleCollectionGroupFolder.FullName -Filter "*.csv" -File
+        $networkRuleCollections = $ruleCollections | where { $_.Name.Split("-")[1] -eq "network" }
+        $applicationRuleCollections = $ruleCollections | where { $_.Name.Split("-")[1] -eq "application" }
+
+        $ruleCollection= @()
+        foreach ($networkRuleCollection in $networkRuleCollections) {
+            $rulesCsv = Import-Csv -Path $networkRuleCollections.FullName
+
+            $rules = @()
+
+            $rules += $rulesCsv | ForEach-Object {
+                [PSCustomObject]@{
+                    RuleType            = 'NetworkRule'
+                    Name                = $_.Name
+                    Description         = $_.Description
+                    SourceAddresses     = $_.Source -split ';'
+                    DestinationAddresses = $_.Destination -split ';'
+                    DestinationPorts    = $_.Port -split ';'
+                    IpProtocols         = $_.Protocol -split ';'
+                }
+            }
+            $ruleCollection += [PSCustomObject]@{
+                Name                = $networkRuleCollections.Name.Split("-")[3]
+                RuleCollectionType = 'FirewallPolicyFilterRuleCollection'
+                Priority            = $networkRuleCollection.Name.Split("-")[0]
+                Action              = [PSCustomObject]@{ Type = $networkRuleCollection.Name.Split("-")[2] }
+                Rules               = $rules
+            }
+        }
+
+        $ruleCollectionGroup = [PSCustomObject]@{
+            Name       = $RuleCollectionGroupName
+            Properties = [PSCustomObject]@{
+                Priority         = $RuleCollectionGroupPriority
+                RuleCollections  = $ruleCollection
+            }
+        }
+
+        $ruleCollectionGroups += $ruleCollectionGroup
+    }
+
+    return $ruleCollectionGroups
 }

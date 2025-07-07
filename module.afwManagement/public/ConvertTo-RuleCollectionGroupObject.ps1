5bf82271560eb1206cac7d6591989f2fb18cecdc
Niklas Klasen <klasen.niklas@gmail.com>
2025-07-07 23:12:22 +0200

    add INFO output

--- module.afwManagement/public/ConvertTo-RuleCollectionGroupObject.ps1
+++ module.afwManagement/public/ConvertTo-RuleCollectionGroupObject.ps1
@@ -4,20 +4,23 @@ function ConvertTo-RuleCollectionGroupObject {
         [Parameter(Mandatory = $true)]
         [string]$FirewallRulesFolderPath
     )
-
+    Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Getting firewall rules folder from path: $FirewallRulesFolderPath"
     $ruleCollectionGroupFolders = Get-ChildItem -Path $FirewallRulesFolderPath -Directory -Recurse
 
     $ruleCollectionGroups = @() 
     foreach ($ruleCollectionGroup in $ruleCollectionGroupFolders) {
         $RuleCollectionGroupName = $ruleCollectionGroup.Name.Split("-")[1]
         $RuleCollectionGroupPriority = $ruleCollectionGroup.Name.Split("-")[0]
-
+        
+        Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Creating rule collection group: $ruleCollectionGroupName"
+        
         $ruleCollections = Get-ChildItem -Path $ruleCollectionGroup.FullName -Filter "*.csv" -File
         $networkRuleCollections = $ruleCollections | where { $_.Name.Split("-")[1] -eq "network" }
         $applicationRuleCollections = $ruleCollections | where { $_.Name.Split("-")[1] -eq "application" }
 
         $ruleCollection= @()
         foreach ($networkRuleCollection in $networkRuleCollections) {
+            Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Creating rule collection $($networkRuleCollection.Name) to rule collection group $RuleCollectionGroupName"
             $rulesCsv = Import-Csv -Path $networkRuleCollection.FullName
 
             $rules = @()
@@ -39,6 +42,7 @@ function ConvertTo-RuleCollectionGroupObject {
                 Action              = [PSCustomObject]@{ Type = $networkRuleCollection.Name.Split("-")[2] }
                 Rules               = $rules
             }
+            Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Rule collection $($networkRuleCollection.Name) added to rule collection group $RuleCollectionGroupName"
         }
 
         $ruleCollectionGroup = [PSCustomObject]@{
@@ -50,7 +54,10 @@ function ConvertTo-RuleCollectionGroupObject {
         }
 
         $ruleCollectionGroups += $ruleCollectionGroup
+
+        Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Rule collection group: $ruleCollectionGroupName created"
     }
 
+    Write-Host "[$(Get-Date -UFormat "%Y-%m-%d %H:%M:%S%Z")] INFO: Rule collection groups created"
     return $ruleCollectionGroups
 }
